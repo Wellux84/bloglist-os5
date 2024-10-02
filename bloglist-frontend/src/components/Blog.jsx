588e99d8c9c8388cf01774322d19d4b59ae57f20
Wellux84 <jullesammakka@hotmail.fi>
2024-10-02 23:11:18 +0300

    5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  const toggleAll = () => {
+    setShowAll(!showAll)
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleAll}>
+          {showAll ? 'hide' : 'view'}
+        </button>
+      </div>
+      {showAll && (
+        <div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes} <button>like</button></div>
+          <div>added by {blog.user.name}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
